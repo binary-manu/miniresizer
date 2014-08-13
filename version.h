e7d527dba7548204bebc9cea1441aae12b306894
binary-manu <47382117+binary-manu@users.noreply.github.com>
2014-08-13 15:35:42 +0200

    Inflate version to 0.0.3

--- version.h
+++ version.h
@@ -4,8 +4,8 @@
 #define APP_NAME			"MiniResizer"
 #define VERSION_MAJOR		0
 #define VERSION_MINOR		0
-#define VERSION_REV			2
-#define VERSION_STRING		"0.0.2"
+#define VERSION_REV			3
+#define VERSION_STRING		"0.0.3"
 #define APP_VERSIONED_NAME	APP_NAME " " VERSION_STRING
 
 #endif	/* VERSION_H */
