ddce77bec2c996c61314578dc78449496f3f1196
binary-manu <47382117+binary-manu@users.noreply.github.com>
2019-12-28 20:09:05 +0100

    Rewrite test to not require running executables
    Mainly to avoid running things when cross-compiling.

--- src/cmake_modules/FFMPEGNewAVCodecAPI.cmake
+++ src/cmake_modules/FFMPEGNewAVCodecAPI.cmake
@@ -1,21 +1,10 @@
-include(CheckCXXSourceRuns)
+include(CheckSymbolExists)
 
 if(NOT DEFINED FFMPEG_USE_NEW_AVCODEC_API)
     message(STATUS "Checking if we should use the new packet decoding API")
     get_target_property(CMAKE_REQUIRED_INCLUDES FFMPEG_avcodec INTERFACE_INCLUDE_DIRECTORIES)
     get_target_property(CMAKE_REQUIRED_LIBRARIES FFMPEG_avcodec INTERFACE_LINK_LIBRARIES)
-    check_cxx_source_runs(
-        [[
-            extern "C" {
-                #include <libavcodec/version.h>
-            }
-            int main() {
-                return !(LIBAVCODEC_VERSION_MAJOR >= 57 && LIBAVCODEC_VERSION_MINOR >= 48);
-            }
-        ]]
-
-        FFMPEG_USE_NEW_AVCODEC_API
-    )
+    check_symbol_exists("avcodec_send_packet" "libavcodec/avcodec.h" FFMPEG_USE_NEW_AVCODEC_API)
     if(FFMPEG_USE_NEW_AVCODEC_API) 
         message(STATUS "Checking if we should use the new packet decoding API - yes")
     else()
